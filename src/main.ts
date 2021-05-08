f70523df1966cd24606992510690095d84389941
TheNightmareX <TheNightmareX@foxmail.com>
2021-05-08 14:32:18 +0800

    refactor: remove global use of ValidationPipe
    For that is already used in "nest-restful"

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/jwt.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector)));
   await app.listen(3000);
 }
--- test/utils/prepare-e2e.ts
+++ test/utils/prepare-e2e.ts
@@ -1,4 +1,3 @@
-import { ValidationPipe } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { TestingModule } from '@nestjs/testing';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
@@ -6,7 +5,6 @@ import supertest from 'supertest';
 
 export async function prepareE2E(module: TestingModule) {
   const app = module.createNestApplication();
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector)));
   await app.init();
   const requester = supertest(app.getHttpServer());
