d36088392e1c1ac7c26c695f2dfa460f043382fb
TheNightmareX <TheNightmareX@foxmail.com>
2021-05-08 11:55:22 +0800

    refactor: remove confusing module "config.ts"

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import './config';
+import { config } from 'dotenv';
+
+config();
 
 export function getTypeOrmRootModule(debug = false) {
   return TypeOrmModule.forRoot({
--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,13 @@ export const TOKEN_VALIDITY_PERIOD = '4h';
 
 export const PAGINATION_DEFAULT_LIMIT = 20;
 export const PAGINATION_MAX_LIMIT = 50;
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB_PATH: string;
+      SECRET_KEY: string;
+    }
+  }
+}
