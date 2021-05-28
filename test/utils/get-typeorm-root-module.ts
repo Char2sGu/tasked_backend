86bcb94c49a3c70d0559292586163c28321acae2
TheNightmareX <TheNightmareX@foxmail.com>
2021-05-28 21:48:14 +0800

    test: update utils

--- test/utils/get-typeorm-root-module.ts
+++ test/utils/get-typeorm-root-module.ts
@@ -1,10 +1,12 @@
+import { Type } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-export function getTypeOrmRootModule() {
+export function getTypeOrmRootModule(...entities: Type[]) {
   return TypeOrmModule.forRoot({
     type: 'sqlite',
     database: ':memory:',
     autoLoadEntities: true,
+    entities: entities,
     synchronize: true,
     keepConnectionAlive: true,
   });
