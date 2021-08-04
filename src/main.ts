e6c38a6299558d761e08edcdf8de3b0e7a735da4
TheNightmareX <TheNightmareX@foxmail.com>
2021-08-04 18:11:31 +0800

    fix: use `AppModule` as the DI container of class-validator

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   // https://github.com/nestjs/nest/issues/528#issuecomment-403212561
-  useContainer(app, { fallbackOnErrors: true });
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 }
 bootstrap();
