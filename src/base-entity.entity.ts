fb7e2fdbe4744dab1246971eec742d61d79daf10
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-11 18:27:28 +0800

    feat: make `BaseEntity` also a base GraphQL object type

--- src/base-entity.entity.ts
+++ src/base-entity.entity.ts
@@ -1,14 +1,18 @@
 import { BaseEntity as Base, PrimaryKey, Property } from '@mikro-orm/core';
+import { Field, ID } from '@nestjs/graphql';
 
 export class BaseEntity<T extends BaseEntity<T>> extends Base<T, 'id'> {
+  @Field(() => ID)
   @PrimaryKey()
   readonly id: number;
 
+  @Field(() => Date)
   @Property({
     onCreate: () => new Date(),
   })
   readonly createdAt: Date;
 
+  @Field(() => Date)
   @Property({
     onCreate: () => new Date(),
     onUpdate: () => new Date(),
