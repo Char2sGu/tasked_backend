fd2ce711dcfbc256a6e5e49a2cb2b6250c4fdf5c
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-13 21:44:18 +0800

    feat: apply a global `ValidationPipe`

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 
@@ -5,7 +6,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
   await app.listen(3000);
+
   // https://github.com/nestjs/nest/issues/528#issuecomment-403212561
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 }
