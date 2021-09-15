6adacc32f5be02b39af6aec163445c864c8fcc5a
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-16 01:37:29 +0800

    fix: add missing `@Allow()` for `AuthArgs`

--- src/auth/dto/auth.args.ts
+++ src/auth/dto/auth.args.ts
@@ -1,10 +1,13 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { Allow } from 'class-validator';
 
 @ArgsType()
 export class AuthArgs {
   @Field(() => String)
+  @Allow()
   username: string;
 
   @Field(() => String)
+  @Allow()
   password: string;
 }
