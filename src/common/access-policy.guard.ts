069a0fd1a74bfb5fe9b020aba0aa6eba134da8a5
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-16 01:31:06 +0800

    refactor: make `AccessPolicyGuard` compatible for both GraphQL and REST

--- src/common/access-policy.guard.ts
+++ src/common/access-policy.guard.ts
@@ -1,19 +1,21 @@
 import { ExecutionContext } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { ExpressContext } from 'apollo-server-express';
 import { AccessPolicyGuard as BaseAccessPolicyGuard } from 'nest-access-policy';
 
 export class AccessPolicyGuard extends BaseAccessPolicyGuard {
   async canActivate(context: ExecutionContext) {
-    const gqlContext = GqlExecutionContext.create(context);
+    if (context.getType<GqlContextType>() == 'graphql') {
+      const gqlContext = GqlExecutionContext.create(context);
 
-    const request = gqlContext.getContext<ExpressContext>().req;
-    request.params = gqlContext.getArgs();
+      const request = gqlContext.getContext<ExpressContext>().req;
+      request.params = gqlContext.getArgs();
 
-    const httpArgumentsHost = context.switchToHttp();
-    httpArgumentsHost.getRequest = () => request as any;
+      const httpArgumentsHost = context.switchToHttp();
+      httpArgumentsHost.getRequest = () => request as any;
 
-    context.switchToHttp = () => httpArgumentsHost;
+      context.switchToHttp = () => httpArgumentsHost;
+    }
 
     return super.canActivate(context);
   }
