a4e391eeab9c092c89db1124d1fc3cca614b8ec6
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-19 13:14:27 +0800

    fix: add the missing `@InputType()`

--- src/classrooms/dto/classroom-update.input.ts
+++ src/classrooms/dto/classroom-update.input.ts
@@ -1,5 +1,6 @@
-import { PartialType } from '@nestjs/graphql';
+import { InputType, PartialType } from '@nestjs/graphql';
 
 import { ClassroomCreateInput } from './classroom-create.input';
 
+@InputType()
 export class ClassroomUpdateInput extends PartialType(ClassroomCreateInput) {}
