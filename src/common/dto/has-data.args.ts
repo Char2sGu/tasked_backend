c9faad101345e55355aa138f44c452b1f9d72af2
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-24 22:58:12 +0800

    refactor: make `HasDataArgs` no more extend useless base classes

--- src/common/dto/has-data.args.ts
+++ src/common/dto/has-data.args.ts
@@ -2,10 +2,9 @@ import { Type } from '@nestjs/common';
 import { ArgsType } from '@nestjs/graphql';
 
 import { Field } from '../field.decorator';
-import { ValidationContextAttached } from '../validation/validation-context-attached.dto';
 
 @ArgsType()
-export class HasDataArgs<Data> extends ValidationContextAttached {
+export class HasDataArgs<Data> {
   static for<Data>(type: Type<Data>): Type<HasDataArgs<Data>> {
     @ArgsType()
     class Args extends this<Data> {
