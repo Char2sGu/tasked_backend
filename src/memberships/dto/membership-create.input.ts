7c8a8501a7efb197750d2f17ca94585c226d580c
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-24 22:44:16 +0800

    refactor: remove the useless `MembershipCreateInput`

--- src/memberships/dto/membership-create.input.ts
+++
@@ -1,5 +0,0 @@
-import { InputType } from '@nestjs/graphql';
-import { ValidationContextAttached } from 'src/common/validation/validation-context-attached.dto';
-
-@InputType()
-export class MembershipCreateInput extends ValidationContextAttached {}
