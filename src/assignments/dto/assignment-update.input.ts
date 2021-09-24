3e37b089089b5a048294b02dd52adaed76dff0fd
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-24 17:51:35 +0800

    fix: add missing `@InputType()` for `AssignmentUpdateInput`

--- src/assignments/dto/assignment-update.input.ts
+++ src/assignments/dto/assignment-update.input.ts
@@ -1,7 +1,8 @@
-import { PartialType, PickType } from '@nestjs/graphql';
+import { InputType, PartialType, PickType } from '@nestjs/graphql';
 
 import { AssignmentCreateInput } from './assignment-create.input';
 
+@InputType()
 export class AssignmentUpdateInput extends PartialType(
   PickType(AssignmentCreateInput, ['_context', 'isPublic'] as const),
 ) {}
