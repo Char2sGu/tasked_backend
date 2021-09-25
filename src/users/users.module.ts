f690f590f046db4b6215654a310500ee9ee41ed7
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-26 01:45:28 +0800

    feat: implement relation fields for `UsersModule`

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,11 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AccessPolicyModule } from 'nest-access-policy';
+import { AssignmentsModule } from 'src/assignments/assignments.module';
+import { ClassroomsModule } from 'src/classrooms/classrooms.module';
+import { JoinApplicationsModule } from 'src/join-applications/join-applications.module';
+import { MembershipsModule } from 'src/memberships/memberships.module';
+import { TasksModule } from 'src/tasks/tasks.module';
 
 import { User } from './entities/user.entity';
 import { UsersAccessPolicy } from './users.access-policy';
@@ -8,7 +13,15 @@ import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([User]), AccessPolicyModule],
+  imports: [
+    MikroOrmModule.forFeature([User]),
+    AccessPolicyModule,
+    forwardRef(() => ClassroomsModule),
+    forwardRef(() => JoinApplicationsModule),
+    forwardRef(() => MembershipsModule),
+    forwardRef(() => TasksModule),
+    forwardRef(() => AssignmentsModule),
+  ],
   providers: [UsersResolver, UsersService, UsersAccessPolicy],
   exports: [UsersService],
 })
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,12 +1,22 @@
 import { Inject, UseGuards } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Parent, Query, Resolver } from '@nestjs/graphql';
 import { UseAccessPolicies } from 'nest-access-policy';
+import { AssignmentsService } from 'src/assignments/assignments.service';
+import { QueryAssignmentsArgs } from 'src/assignments/dto/query-assignments.args';
+import { ClassroomsService } from 'src/classrooms/classrooms.service';
+import { QueryClassroomsArgs } from 'src/classrooms/dto/query-classrooms.args';
 import { AccessPolicyGuard } from 'src/common/access-policy/access-policy.guard';
 import { SkipAuth } from 'src/common/auth/skip-auth.decorator';
 import { CRUD_FILTERS } from 'src/common/crud-filters/crud-filters.token';
 import { CrudFilters } from 'src/common/crud-filters/crud-filters.type';
 import { FlushDb } from 'src/common/flush-db/flush-db.decorator';
 import { ReqUser } from 'src/common/req-user.decorator';
+import { ResolveField } from 'src/common/resolve-field.decorator';
+import { QueryJoinApplicationsArgs } from 'src/join-applications/dto/query-join-applications.args';
+import { QueryMembershipsArgs } from 'src/memberships/dto/query-memberships.args';
+import { MembershipsService } from 'src/memberships/memberships.service';
+import { QueryTasksArgs } from 'src/tasks/dto/query-tasks.args';
+import { TasksService } from 'src/tasks/tasks.service';
 
 import { CreateUserArgs } from './dto/create-user.args';
 import { PaginatedUsers } from './dto/paginated-users.dto';
@@ -26,6 +36,18 @@ export class UsersResolver {
 
   @Inject(CRUD_FILTERS)
   private readonly filters: CrudFilters;
+
+  @Inject()
+  private readonly classroomsService: ClassroomsService;
+
+  @Inject()
+  private readonly membershipsService: MembershipsService;
+
+  @Inject()
+  private readonly tasksService: TasksService;
+
+  @Inject()
+  private readonly assignmentsService: AssignmentsService;
 
   @Query(() => PaginatedUsers, { name: 'users' })
   async queryMany(
@@ -59,5 +81,65 @@ export class UsersResolver {
   @Mutation(() => User, { name: 'updateUser' })
   async updateOne(@ReqUser() user: User, @Args() { id, data }: UpdateUserArgs) {
     return this.service.update(id, data, { filters: this.filters(user) });
+  }
+
+  @ResolveField(() => User, 'classrooms')
+  async resolveClassrooms(
+    @ReqUser() user: User,
+    @Parent() entity: User,
+    @Args() { limit, offset }: QueryClassroomsArgs,
+  ) {
+    return this.classroomsService.list(
+      { creator: entity },
+      { limit, offset, filters: this.filters(user) },
+    );
+  }
+
+  @ResolveField(() => User, 'joinApplications')
+  async resolveJoinApplications(
+    @ReqUser() user: User,
+    @Parent() entity: User,
+    @Args() { limit, offset }: QueryJoinApplicationsArgs,
+  ) {
+    return this.membershipsService.list(
+      { owner: entity },
+      { limit, offset, filters: this.filters(user) },
+    );
+  }
+
+  @ResolveField(() => User, 'memberships')
+  async resolveMemberships(
+    @ReqUser() user: User,
+    @Parent() entity: User,
+    @Args() { limit, offset }: QueryMembershipsArgs,
+  ) {
+    return this.membershipsService.list(
+      { owner: entity },
+      { limit, offset, filters: this.filters(user) },
+    );
+  }
+
+  @ResolveField(() => User, 'tasks')
+  async resolveTasks(
+    @ReqUser() user: User,
+    @Parent() entity: User,
+    @Args() { limit, offset }: QueryTasksArgs,
+  ) {
+    return this.tasksService.list(
+      { creator: entity },
+      { limit, offset, filters: this.filters(user) },
+    );
+  }
+
+  @ResolveField(() => User, 'assignments')
+  async resolveAssignments(
+    @ReqUser() user: User,
+    @Parent() entity: User,
+    @Args() { limit, offset }: QueryAssignmentsArgs,
+  ) {
+    return this.assignmentsService.list(
+      { recipient: entity },
+      { limit, offset, filters: this.filters(user) },
+    );
   }
 }
