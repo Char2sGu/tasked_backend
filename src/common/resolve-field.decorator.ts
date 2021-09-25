fcde3eaefeaaa21dd41d7bdd4406bce0436d7170
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-25 08:50:06 +0800

    feat: create strictly typed `@ResolveField()`

---
+++ src/common/resolve-field.decorator.ts
@@ -0,0 +1,37 @@
+import { NonFunctionPropertyNames } from '@mikro-orm/core/typings';
+import { Type } from '@nestjs/common';
+import {
+  ResolveField as ResolveFieldBase,
+  ResolveFieldOptions,
+  ReturnTypeFunc,
+} from '@nestjs/graphql';
+
+/**
+ * Provide better type support.
+ * @param entityType
+ * @param field
+ * @param returnType
+ * @param options
+ * @returns
+ */
+export const ResolveField =
+  <Entity, Field extends Extract<NonFunctionPropertyNames<Entity>, string>>(
+    entityType: () => Type<Entity>,
+    field: Field,
+    returnType?: ReturnTypeFunc,
+    options?: ResolveFieldOptions,
+  ) =>
+  (
+    prototype: unknown,
+    propertyKey: `resolve${Capitalize<Field>}`,
+    descriptor: PropertyDescriptor,
+  ) =>
+    ResolveFieldBase(field, returnType, options)(
+      propertyKey,
+      propertyKey,
+      descriptor,
+    );
+
+type Capitalize<T extends string> = T extends `${infer A}${infer B}`
+  ? `${Uppercase<A>}${B}`
+  : never;
