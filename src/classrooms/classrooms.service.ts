9ff5633727646dabdef7d08744f7b6f73a4e07c0
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-30 00:34:13 +0800

    fix: soft-delete classrooms

--- src/classrooms/classrooms.service.ts
+++ src/classrooms/classrooms.service.ts
@@ -1,7 +1,17 @@
+import { FilterQuery, FindOneOrFailOptions } from '@mikro-orm/core';
 import { Injectable } from '@nestjs/common';
 import { CrudService } from 'src/common/crud/crud.service';
 
 import { Classroom } from './entities/classroom.entity';
 
 @Injectable()
-export class ClassroomsService extends CrudService.of(Classroom) {}
+export class ClassroomsService extends CrudService.of(Classroom) {
+  async destroy(
+    where: FilterQuery<Classroom>,
+    options?: FindOneOrFailOptions<Classroom>,
+  ) {
+    const entity = await this.retrieve(where, options);
+    entity.deletedAt = new Date();
+    return entity;
+  }
+}
