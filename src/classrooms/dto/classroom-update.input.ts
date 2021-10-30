6064bd55b4326125dfd0b1a880f89948bc580c62
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-31 00:27:13 +0800

    feat(classrooms): implement update of `isOpen`

--- src/classrooms/dto/classroom-update.input.ts
+++ src/classrooms/dto/classroom-update.input.ts
@@ -1,6 +1,10 @@
 import { InputType, PartialType } from '@nestjs/graphql';
+import { Field } from 'src/common/utilities/field.decorator';
 
 import { ClassroomCreateInput } from './classroom-create.input';
 
 @InputType()
-export class ClassroomUpdateInput extends PartialType(ClassroomCreateInput) {}
+export class ClassroomUpdateInput extends PartialType(ClassroomCreateInput) {
+  @Field(() => Boolean, { nullable: true })
+  isOpen?: boolean;
+}
