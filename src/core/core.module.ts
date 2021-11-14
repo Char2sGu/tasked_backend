eb6219b027fbe1bfff625ddfb8a989690d26f1ba
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-14 17:21:32 +0800

    chore(core,shared): add comments on the responsiblities of these two module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,6 +3,9 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 
 import { FlushDbInterceptor } from './flush-db.interceptor';
 
+/**
+ * Provide core providers and should only be imported in the {@link AppModule}.
+ */
 @Module({
   providers: [
     {
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ValidationModule } from 'src/validation/validation.module';
 
+/**
+ * provide shared providers for every feature providers.
+ */
 @Module({
   providers: [ValidationModule],
   exports: [ValidationModule],
