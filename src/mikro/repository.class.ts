c1e210afeb4904af3c9c02dc91e4215d4a2e6914
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-16 00:12:56 +0800

    feat(mikro/repo): add method `.findAndPaginate()`

--- src/mikro/repository.class.ts
+++ src/mikro/repository.class.ts
@@ -1,9 +1,23 @@
-import { EntityData, New } from '@mikro-orm/core';
+import {
+  EntityData,
+  FilterQuery,
+  FindOptions,
+  Loaded,
+  New,
+} from '@mikro-orm/core';
 import { EntityRepository } from '@mikro-orm/knex';
 
 import { SOFT_DELETABLE } from './soft-deletable.symbol';
 
 export class Repository<Entity> extends EntityRepository<Entity> {
+  async findAndPaginate<Population extends string = never>(
+    where: FilterQuery<Entity>,
+    options?: FindOptions<Entity, Population>,
+  ): Promise<{ total: number; results: Loaded<Entity, Population>[] }> {
+    const [results, total] = await super.findAndCount(where, options);
+    return { total, results };
+  }
+
   create<Population extends string = any>(
     data: EntityData<Entity>,
     persist = true,
