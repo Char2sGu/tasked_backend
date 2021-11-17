b16d0e56ab404af82929eecadf2c5158796e166f
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-18 00:37:26 +0800

    refactor(validation/pipe): use an own derived pipe

--- src/validation/validation.module.ts
+++ src/validation/validation.module.ts
@@ -1,20 +1,14 @@
-import { Module, ValidationPipe } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 
 import { IsPrimaryKeyConstraint } from './is-primary-key.constraint';
+import { ValidationPipe } from './validation.pipe';
 
 @Module({
   providers: [
     {
       provide: APP_PIPE,
-      useValue: new ValidationPipe({
-        transform: true,
-        transformOptions: {
-          exposeDefaultValues: true, // `@Field(..., { defaultValue: ... })` cannot work in `@ResolveField()` (Bug)
-          exposeUnsetFields: false, // if `true`, update actions will unexpectedly assign an `undefined` value to the entity fields and cause error
-        },
-        whitelist: true,
-      }),
+      useClass: ValidationPipe,
     },
     IsPrimaryKeyConstraint,
   ],
