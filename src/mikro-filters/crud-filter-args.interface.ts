fd487350dbdc5bd40535f2094850d3a5b8a04e40
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-18 20:12:11 +0800

    refactor(mikro-filters): remove the interceptor

--- src/mikro-filters/crud-filter-args.interface.ts
+++
@@ -1,5 +0,0 @@
-import { User } from 'src/users/entities/user.entity';
-
-export interface CrudFilterArgs {
-  user: User;
-}
--- src/mikro-filters/mikro-filters.module.ts
+++ src/mikro-filters/mikro-filters.module.ts
@@ -1,19 +1,11 @@
 import { DynamicModule, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-
-import { MikroFiltersInterceptor } from './mikro-filters.interceptor';
 
 @Module({})
 export class MikroFiltersModule {
   static forRoot(): DynamicModule {
     return {
       module: MikroFiltersModule,
-      providers: [
-        {
-          provide: APP_INTERCEPTOR,
-          useClass: MikroFiltersInterceptor,
-        },
-      ],
+      providers: [],
     };
   }
 }
