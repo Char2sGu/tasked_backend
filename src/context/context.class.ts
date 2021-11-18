e38c0d0eebf3de8ad56eabf5bc93a324d20bef51
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-18 19:48:01 +0800

    feat(context): adjust the context content

--- src/context/context.class.ts
+++ src/context/context.class.ts
@@ -1,14 +1,19 @@
 import { AsyncLocalStorage } from 'async_hooks';
 import { Request } from 'express';
+import { User } from 'src/users/entities/user.entity';
 
 export class Context {
-  private static storage = new AsyncLocalStorage<Context>();
-
   static get current() {
     return this.storage.getStore();
   }
 
-  constructor(public request: Request) {}
+  private static storage = new AsyncLocalStorage<Context>();
+
+  public user?: User;
+
+  constructor(request: Request) {
+    this.user = request.user;
+  }
 
   apply<T>(fn: () => T) {
     return Context.storage.run(this, fn);
