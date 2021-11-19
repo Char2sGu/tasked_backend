ee8453f8eefb97d32cb045520b166396f6f36ff3
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-19 18:11:35 +0800

    refactor(context/core): rename to context/middleware-module

--- src/context/context-middleware.module.ts
+++ src/context/context-middleware.module.ts
@@ -3,7 +3,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ContextMiddleware } from './context.middleware';
 
 @Module({})
-export class ContextCoreModule implements NestModule {
+export class ContextMiddlewareModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(ContextMiddleware).forRoutes('*');
   }
--- src/context/context.module.ts
+++ src/context/context.module.ts
@@ -1,13 +1,13 @@
 import { DynamicModule, Module } from '@nestjs/common';
 
-import { ContextCoreModule } from './context-core.module';
+import { ContextMiddlewareModule } from './context-middleware.module';
 
 @Module({})
 export class ContextModule {
   static forRoot(): DynamicModule {
     return {
       module: ContextModule,
-      imports: [ContextCoreModule],
+      imports: [ContextMiddlewareModule],
     };
   }
 }
