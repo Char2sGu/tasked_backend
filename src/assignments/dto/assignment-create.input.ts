90440cfbd00abfaa06f9f0ace6ab36a700794163
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-20 20:16:21 +0800

    fix(assignments/inputs): fix `recipient` validation

--- src/assignments/dto/assignment-create.input.ts
+++ src/assignments/dto/assignment-create.input.ts
@@ -1,14 +1,14 @@
 import { ID, InputType } from '@nestjs/graphql';
 import { CommonFilter } from 'src/common/common-filter.enum';
 import { Field } from 'src/common/field.decorator';
+import { Membership } from 'src/memberships/entities/membership.entity';
 import { Task } from 'src/tasks/entities/task.entity';
-import { User } from 'src/users/entities/user.entity';
 import { IsPrimaryKey } from 'src/validation/is-primary-key.decorator';
 
 @InputType()
 export class AssignmentCreateInput {
   @Field(() => ID)
-  @IsPrimaryKey(() => User, [CommonFilter.Crud])
+  @IsPrimaryKey(() => Membership, [CommonFilter.Crud])
   recipient: number;
 
   @Field(() => ID)
