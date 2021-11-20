79510e982a4a7170535529a2459cb252c482dcab
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-20 16:44:19 +0800

    fix(.d.ts): fix the env declaration

--- src/declare-env.d.ts
+++ src/declare-env.d.ts
@@ -1,9 +1,7 @@
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace NodeJS {
-    interface ProcessEnv {
-      DB_PATH: string;
-      SECRET_KEY: string;
-    }
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DB_PATH: string;
+    SECRET_KEY: string;
   }
 }
