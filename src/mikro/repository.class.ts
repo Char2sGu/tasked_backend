363f5eddb86e4f1c25b3dab63abbde96b335e6d6
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-30 22:25:32 +0800

    feat(mikro/repo): add method `.findOneById()`

--- src/mikro/repository.class.ts
+++ src/mikro/repository.class.ts
@@ -2,12 +2,22 @@ import {
   EntityData,
   EntityRepository,
   FilterQuery,
+  FindOneOptions,
   FindOptions,
   Loaded,
   New,
+  Primary,
+  Utils,
 } from '@mikro-orm/core';
 
 export class Repository<Entity> extends EntityRepository<Entity> {
+  /**
+   * Replace the return value of {@link EntityRepository.findAndCount} with an
+   * object.
+   * @param where
+   * @param options
+   * @returns
+   */
   async findAndPaginate<Population extends string = never>(
     where: FilterQuery<Entity>,
     options?: FindOptions<Entity, Population>,
@@ -16,6 +26,25 @@ export class Repository<Entity> extends EntityRepository<Entity> {
     return { total, results };
   }
 
+  /**
+   * Use the Identity Map if possible, and execute a query when the entity is
+   * not loaded in the Identity Map.
+   * @param id
+   * @returns
+   */
+  async findOneById(id: Primary<Entity>, options?: FindOneOptions<Entity>) {
+    const loaded = this.em
+      .getUnitOfWork()
+      .getById<Entity>(Utils.className(this.entityName), id);
+    return loaded ?? this.findOne(id as any, options);
+  }
+
+  /**
+   * Persist the created entity by default.
+   * @param data
+   * @param persist
+   * @returns
+   */
   create<Population extends string = any>(
     data: EntityData<Entity>,
     persist = true,
@@ -25,6 +54,11 @@ export class Repository<Entity> extends EntityRepository<Entity> {
     return result;
   }
 
+  /**
+   * Returns the removed entity rather than `this`.
+   * @param entity
+   * @returns
+   */
   delete(entity: Entity) {
     this.remove(entity);
     return entity;
