e5b64f691fa835f390784416facf06d5fbc7e377
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-01 22:41:37 +0800

    refactor(core): extract graphql module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { ContextModule } from 'src/context/context.module';
-import { DEBUG } from 'src/env.constants';
 import { MikroModule } from 'src/mikro/mikro.module';
 import { ThrottlerModule } from 'src/throttler/throttler.module';
 import { ValidationModule } from 'src/validation/validation.module';
+
+import { GraphqlModule } from '../graphql/graphql.module';
 
 /**
  * Provide core providers and should only be imported in the {@link AppModule}.
@@ -13,10 +13,7 @@ import { ValidationModule } from 'src/validation/validation.module';
   imports: [
     ContextModule.forRoot(),
     MikroModule.forRoot(),
-    GraphQLModule.forRoot({
-      autoSchemaFile: true,
-      playground: DEBUG,
-    }),
+    GraphqlModule,
     ValidationModule.forRoot(),
     ThrottlerModule,
   ],
