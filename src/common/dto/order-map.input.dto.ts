dc4b7b120d05b2499211038fb37a87df342e6f6e
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-04 13:07:21 +0800

    fix(common/order): replace `QueryOrder` with `QueryOrderNumeric`
    The former one will cause error front end codegen.

--- src/common/dto/order-map.input.dto.ts
+++ src/common/dto/order-map.input.dto.ts
@@ -1,9 +1,9 @@
-import { QueryOrder } from '@mikro-orm/core';
+import { QueryOrder, QueryOrderNumeric } from '@mikro-orm/core';
 import { Type } from '@nestjs/common';
 import { InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { Field } from 'src/common/field.decorator';
 
-registerEnumType(QueryOrder, { name: 'QueryOrder' });
+registerEnumType(QueryOrderNumeric, { name: 'QueryOrder' });
 
 @ObjectType()
 export class OrderMap {
@@ -14,12 +14,15 @@ export class OrderMap {
     class _OrderMap extends OrderMap {}
 
     fields.forEach((field) => {
-      Field(() => QueryOrder, { nullable: true })(_OrderMap.prototype, field);
+      Field(() => QueryOrderNumeric, { nullable: true })(
+        _OrderMap.prototype,
+        field,
+      );
     });
     InputType()(_OrderMap);
 
     return _OrderMap as Type<unknown> as Type<Record<Field, QueryOrder>>;
   }
 
-  [field: string]: QueryOrder;
+  [field: string]: QueryOrderNumeric;
 }
