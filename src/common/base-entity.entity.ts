3560f9598fe785b13ffa31a1f238cf2cf4bcbf6a
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-04 17:37:46 +0800

    feat(common/base-entity): make fields orderable

--- src/common/base-entity.entity.ts
+++ src/common/base-entity.entity.ts
@@ -2,6 +2,7 @@ import { BaseEntity as Base, PrimaryKey, Property } from '@mikro-orm/core';
 import { ID, ObjectType } from '@nestjs/graphql';
 import { SoftDeletable } from 'src/mikro/soft-deletable/soft-deletable.decorator';
 
+import { Orderable } from './dto/order/orderable.decorator';
 import { Field } from './field.decorator';
 
 /**
@@ -11,14 +12,17 @@ import { Field } from './field.decorator';
 @ObjectType()
 @SoftDeletable(() => BaseEntity, 'deletedAt', () => new Date())
 export class BaseEntity<T extends BaseEntity<T>> extends Base<T, 'id'> {
+  @Orderable()
   @Field(() => ID)
   @PrimaryKey()
   readonly id: number;
 
+  @Orderable()
   @Field(() => Date)
   @Property({ onCreate: () => new Date() })
   readonly createdAt: Date;
 
+  @Orderable()
   @Field(() => Date)
   @Property({ onCreate: () => new Date(), onUpdate: () => new Date() })
   readonly updatedAt: Date;
