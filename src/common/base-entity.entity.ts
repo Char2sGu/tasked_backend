2d2e925b209bd2f11e2bc9dfb7e7abc619b844e9
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-04 20:15:27 +0800

    feat(common/base-entity): make fields filterable

--- src/common/base-entity.entity.ts
+++ src/common/base-entity.entity.ts
@@ -11,15 +11,15 @@ import { Field } from './field.decorator';
 @ObjectType()
 @SoftDeletable(() => BaseEntity, 'deletedAt', () => new Date())
 export class BaseEntity<T extends BaseEntity<T>> extends Base<T, 'id'> {
-  @Field(() => ID, { orderable: true })
+  @Field(() => ID, { orderable: true, filterable: true })
   @PrimaryKey()
   readonly id: number;
 
-  @Field(() => Date, { orderable: true })
+  @Field(() => Date, { orderable: true, filterable: true })
   @Property({ onCreate: () => new Date() })
   readonly createdAt: Date;
 
-  @Field(() => Date, { orderable: true })
+  @Field(() => Date, { orderable: true, filterable: true })
   @Property({ onCreate: () => new Date(), onUpdate: () => new Date() })
   readonly updatedAt: Date;
 
