f1b9bca3619c92ba811e0df8a99d8c75015701b4
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-18 02:14:12 +0800

    docs(jsdoc/data-loader): document

--- src/data-loader/data-loader.module.ts
+++ src/data-loader/data-loader.module.ts
@@ -2,6 +2,10 @@ import { DynamicModule, Module } from '@nestjs/common';
 
 import { DataLoaderMiddlewareModule } from './data-loader-middleware.module';
 
+/**
+ * A wrap of the `dataloader` package, which provides class-based usage and
+ * request-scoped cache.
+ */
 @Module({})
 export class DataLoaderModule {
   static forRoot(): DynamicModule {
