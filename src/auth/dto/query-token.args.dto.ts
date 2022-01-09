b4bd8d2be4caf019a6f55dc0c38449e5a82cc526
TheNightmareX <TheNightmareX@foxmail.com>
2022-01-09 15:12:25 +0800

    refactor(auth/dto): remove redundant `@Allow()`

--- src/auth/dto/query-token.args.dto.ts
+++ src/auth/dto/query-token.args.dto.ts
@@ -1,14 +1,11 @@
 import { ArgsType } from '@nestjs/graphql';
-import { Allow } from 'class-validator';
 import { Field } from 'src/common/field.decorator';
 
 @ArgsType()
 export class QueryTokenArgs {
   @Field(() => String)
-  @Allow()
   username: string;
 
   @Field(() => String)
-  @Allow()
   password: string;
 }
