0fcd6df87760f9bfab75d600c6b9c482f52cf91c
Char2s <Char2s@outlook.com>
2022-01-14 22:07:25 +0800

    style(app): add a missing blank line

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { RoomsModule } from './rooms/rooms.module';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { ValidationModule } from './validation/validation.module';
+
 @Module({
   imports: [
     ContextModule.forRoot(),
