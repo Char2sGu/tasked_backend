18549db180c751b172a3916ef706bf08d637dea2
Char2s <Char2s@outlook.com>
2022-01-27 21:44:16 +0800

    refactor(env): optimize implementation

--- src/env.constants.ts
+++ src/env.constants.ts
@@ -1,28 +1,18 @@
 import { config } from 'dotenv';
+import { env } from 'process';
 
 config();
 
-export const PORT = Number(process.env.PORT);
+const num = (v: string) => Number(v);
+const str = (v: string) => v;
+const bool = (v: string) =>
+  v == 'true' ? true : v == 'false' ? false : undefined;
 
-export const DB_PATH = process.env.DB_PATH;
-
-export const SECRET_KEY = process.env.SECRET_KEY;
-
-export const GRAPHQL_FREQUENCY_LIMIT = Number(
-  process.env.GRAPHQL_FREQUENCY_LIMIT,
-);
-
-export const GRAPHQL_FREQUENCY_DURATION = Number(
-  process.env.GRAPHQL_FREQUENCY_DURATION,
-);
-
-export const GRAPHQL_COMPLEXITY = Number(process.env.GRAPHQL_COMPLEXITY);
-
-export const GRAPHQL_DEPTH = Number(process.env.GRAPHQL_DEPTH);
-
-export const DEBUG =
-  process.env.DEBUG == 'true'
-    ? true
-    : process.env.DEBUG == 'false'
-    ? false
-    : undefined;
+export const PORT = num(env.PORT);
+export const DB_PATH = str(env.DB_PATH);
+export const SECRET_KEY = str(env.SECRET_KEY);
+export const GRAPHQL_FREQUENCY_LIMIT = num(env.GRAPHQL_FREQUENCY_LIMIT);
+export const GRAPHQL_FREQUENCY_DURATION = num(env.GRAPHQL_FREQUENCY_DURATION);
+export const GRAPHQL_COMPLEXITY = num(env.GRAPHQL_COMPLEXITY);
+export const GRAPHQL_DEPTH = num(env.GRAPHQL_DEPTH);
+export const DEBUG = bool(env.DEBUG);
