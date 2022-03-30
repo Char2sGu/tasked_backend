0a370228034d29048e96bdee8178dea38aae20e3
Char2s <Char2s@outlook.com>
2022-03-30 21:13:35 +0800

    feat(main): enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { PORT } from './common/env.constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // Use Nest DI in class-validator custom validators.
   // https://github.com/nestjs/nest/issues/528#issuecomment-403212561
